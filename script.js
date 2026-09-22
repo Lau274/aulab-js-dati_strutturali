5ff0989b65392a29537c7be8c2aeda5d74b37542
Lau274 <tonnarellilorenzo724@gmail.com>
2026-09-22 19:11:24 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'],
+];
+
+let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+];
+
+let frase =
+    `${array_1[0][0]} ${array_1[1][0]} ${array_1[0][1]} ${array_2[4][0][0]} ` +
+    `${array_1[0][0]} ${array_1[1][0]} ${array_1[0][1]} ${array_1[1][1]} ` +
+    `${array_1[0][0]} ${array_1[1][0]} ${array_1[0][1]} ${array_1[2][0]} ` +
+    `${array_1[2][1]} ${array_2[3][0]} ${array_2[4][1]} ${array_1[0][2]}`;
+
+console.log(frase.charAt(0).toUpperCase() + frase.slice(1));
